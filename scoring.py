2cc24574cc17d9642366bc01b9aa19f5caf48758
Steve Berkeley <sjberkeley@gmail.com>
2025-01-28 22:07:07 -0800

    update scoring.py for 2025 tables

--- scoring.py
+++ scoring.py
@@ -9,20 +9,32 @@ from PyPDF2 import PdfReader
 def create_handles(gender):
     file_handles = {}     # map
     sprint_hurdles = "110mH"
-    multi = "Decathlon"
+    multi = "Dec."
+    imulti = "Hept. sh"
     if gender == "women":
         sprint_hurdles = "100mH"
-        multi = "Heptathlon"
+        multi = "Hept."
+        imulti = "Pent. sh"
 
-    for event_name in ("100m", "200m", "300m", "400m", "500m", sprint_hurdles, "400mH", "4x100m", "4x200m", "4x400m"):
+    for event_name in ("50m", "55m", "60m", "200m sh", "300m sh", "400m sh", "500m sh"):
+        file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
+    for event_name in ("600m sh", "800m sh", "1000m sh", "1500m sh", "Mile sh", "2000m sh"):
+        file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
+    for event_name in ("3000m sh", "2 Miles sh", "5000m sh"):
+        file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
+    for event_name in ("100m", "200m", "300m", "400m", "500m"):
+        file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
+    for event_name in ("4x100m", "4x200m", "4x200m sh", "4x400m", "4x400m sh", "4x400mix", "4x400mix sh"):
+        file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
+    for event_name in ("50mH", "55mH", "60mH", sprint_hurdles, "400mH"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
     for event_name in ("600m", "800m", "1000m", "1500m", "Mile", "2000m"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
-    for event_name in ("2000mSC", "3000m", "3000mSC", "2 Miles", "5000m", "10,000m"):
+    for event_name in ("2000m SC", "3000m", "3000m SC", "2 Miles", "5000m", "10000m"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
-    for event_name in ("5km", "10km", "15km", "10 Miles", "20km"):
+    for event_name in ("5 km", "10 km", "15 km", "10 Miles", "20 km"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
-    for event_name in ("HM", "25km", "30km", "Marathon", "100km"):
+    for event_name in ("HM", "25 km", "30 km", "Marathon", "100 km"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
     for event_name in ("3000mW", "5000mW", "10,000mW", "15,000mW"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
@@ -30,7 +42,7 @@ def create_handles(gender):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
     for event_name in ("3kmW", "5kmW", "10kmW", "15kmW", "20kmW", "30kmW", "35kmW", "50kmW"):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
-    for event_name in ("HJ", "PV", "LJ", "TJ", "SP", "DT", "HT", "JT", multi):
+    for event_name in ("HJ", "PV", "LJ", "TJ", "SP", "DT", "HT", "JT", multi, imulti):
         file_handles[event_name] = open(event_name + "." + gender + ".scores", "w")
     return file_handles
 
@@ -66,20 +78,27 @@ def extract_text_from_pdf(file_path):
             first_column_is_points = False
             if words[1] == "Points":
                 first_column_is_points = True
-                skip_word = False
+                skip_word = 0
                 col_shift = 0
                 num_columns = 0
                 for word_num in range(2, 20):
-                    if skip_word:
-                        skip_word = False
+                    if skip_word > 0:
+                        skip_word = skip_word - 1
                         continue
                     word = words[word_num]
-                    # the only event names consisting of two words are "2 Miles" and "10 Miles"
-                    if (word == "2" or word == "10") and words[word_num + 1] == "Miles":
-                        word = words[word_num] + " " + words[word_num + 1]
-                        skip_word = True
-                        num_columns = num_columns - 1
-                        col_shift = col_shift + 1
+                    # some event names have 2 or 3 words
+                    if words[word_num + 1] == "Miles" or words[word_num + 1] == "km" or \
+                       words[word_num + 1] == "SC" or words[word_num + 1] == "sh":
+                       word = words[word_num] + " " + words[word_num + 1]
+                       skip_word = skip_word + 1
+                       num_columns = num_columns - 1
+                       col_shift = col_shift + 1
+                       if words[word_num + 2] == "sh":
+                           word = words[word_num] + " " + words[word_num + 1] + " " + words[word_num + 2]
+                           skip_word = skip_word + 1
+                           num_columns = num_columns - 1
+                           col_shift = col_shift + 1
+
                     if word.isdigit():           # it's a score
                         num_columns = num_columns + word_num - 1
                         break
