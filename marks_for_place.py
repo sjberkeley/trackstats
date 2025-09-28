7cfef92f4a04e9c95862bd7f88965dac90793555
Steve Berkeley <sjberkeley@gmail.com>
2025-09-27 22:30:47 -0700

    Ties in marks-for-place

--- marks_for_place.py
+++ marks_for_place.py
@@ -15,14 +15,16 @@ event_name_map = {}
 #file1 = open("marks_for_place", "w")
 
 for gender in ("men", "women"):
-    #if gender == "men":
-        #continue
+    if gender == "women":
+        continue
 
     score_maps = {}
     utils.init_score_maps(event_name_map, score_maps)
     urls = data_source.get_urls(gender, False)
 
     for url in urls:
+        if url != "High jump":
+            continue
         if url == "4x100m relay" or url == "4x400m relay" or url == "mixed 4x400m relay":
             #or url == "50 km race walk" or url == "half-marathon" or url == "20 km race walk":
             #or url == "Javelin throw" \
@@ -39,6 +41,7 @@ for gender in ("men", "women"):
         num_lines = len(lines)
         line_num = 0
         place = 1
+        current_performance = ""
         while line_num < num_lines:
             status, words, processing, line_num = data_source.strip_preamble(lines, line_num, processing)
             if status == 0:
@@ -48,9 +51,16 @@ for gender in ("men", "women"):
 
             # extract performance, name and date (year)
             name, year, performance, nation, this_date, city, position, date, dob, line_num = data_source.get_stats(words, lines, line_num)
-            if position[0:1] == str(place):
-                print(("%3s %9s %25s %50s %11s") % (position[0:1], performance, name, city, date))
+            if place < 10:
+                num_digits = 1
+            else:
+                num_digits = 2
+            if position[0:num_digits] == str(place):
+                print(("%3s %9s %25s %50s %11s") % (position[0:num_digits], performance, name, city, date))
+                current_performance = performance
                 place = place + 1
+            elif position[0:num_digits] == str(place-1) and performance == current_performance:
+                print(("%39s %50s %11s") % (name, city, date))
                 if place > 20:
                     break
 
